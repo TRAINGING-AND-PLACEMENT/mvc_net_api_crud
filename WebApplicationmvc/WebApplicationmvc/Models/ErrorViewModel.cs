a2a93d95eea2d250874d116e93222f2010f7a01d
Suryakant Nayak <107774648+suryakant114@users.noreply.github.com>
2023-03-10 23:44:10 +0530

    Delete WebApplicationmvc directory

--- WebApplicationmvc/WebApplicationmvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationmvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
